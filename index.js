b167ce3d16d8cd052adaa7f3e2cd458f7d961db7
Wol <woldut101@icloud.com>
2024-03-26 12:54:06 +0300

    Adds Phase-1 Arrow Functions

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (a, b) => a / b;
+
+let a = 2000;
+let b = 100;
+
+divide(a,b);
+
+const square = (a) => a * a;
+let x = 2;
+square(x);
+
+const add = (a, b) => a + b;
+let c = 3;
+let d = 4;
+add(c, d);
